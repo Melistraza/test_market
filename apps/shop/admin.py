4957a8f68b988aec9c3917207edd367ef60a2f4e
melistraza <melistraza@gmail.com>
2016-03-02 13:28:30 +0200

    add view_on_site feature for products

--- apps/shop/admin.py
+++ apps/shop/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from apps.shop.models import Product, Comments
+from django.core.urlresolvers import reverse
 
 
 class AdminProduct(admin.ModelAdmin):
     list_display = ('name',)
 
-    # def view_on_site(self, obj):
-    #     return 'http://127.0.0.1:8000/' + reverse('product-details',
-    #                                           kwargs={'slug': obj.pk})
+    def view_on_site(self, obj):
+        return 'http://127.0.0.1:8000' + reverse('product_page', kwargs={'product_slug': obj.slug})
 
 
 class AdminComments(admin.ModelAdmin):
