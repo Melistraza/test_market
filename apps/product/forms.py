604c9a67f13f420562ebe0205f3ff4c54ce7fbee
melistraza <melistraza@gmail.com>
2016-03-09 12:51:15 +0200

    add validator

--- apps/product/forms.py
+++ apps/product/forms.py
@@ -6,3 +6,10 @@ class CommentsForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['text', ]
+
+    def clean_text(self):
+        data = self.cleaned_data['text']
+        if len(data) <= 4:
+            raise forms.ValidationError(
+                "Put a little more about their impressions about the product!")
+        return data
